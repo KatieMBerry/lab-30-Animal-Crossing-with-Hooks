80deefe730e18a0c7e3f624f5aae83aa35fe2eff
Katie <katieberrym@gmail.com>
2021-02-08 14:50:11 -0800

    added container and test

---
+++ src/containers/AllVillagers.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const AllVillagers = () => {
+    const [loading, setLoading] = useState(true);
+    const [villagers, setVillagers] = useState([]);
+
+    return (
+        <h1>Loading</h1>
+    );
+
+};
+
+export default AllVillagers;
